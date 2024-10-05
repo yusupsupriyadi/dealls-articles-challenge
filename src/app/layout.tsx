417b068b2aac0c3f3b742428e6f7dad465b1ca32
yusup supriyadi <yusupsupriyadistudio@gmail.com>
2024-10-05 23:59:19 +0700

    refactor(layout): main layout ui adjust

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,41 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import Navbar from '@/components/Navbar';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: "Dealls Articles",
-    template: "%s | Dealls Articles",
-  },
-  description: "Dealls Articles come here",
+	title: {
+		default: 'Dealls Articles',
+		template: '%s | Dealls Articles',
+	},
+	description: 'Dealls Articles come here',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<Navbar />
+				<main className='container mx-auto px-6 lg:px-0'>
+					{children}
+				</main>
+			</body>
+		</html>
+	);
 }
