f56411101cbbda5c11da4c522fb51936caec20cb
yusup supriyadi <yusupsupriyadistudio@gmail.com>
2024-10-06 19:11:55 +0700

    refactor: add verification google search for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
 	twitter: {
 		card: 'summary_large_image',
 	},
+	verification: {
+		google: 'NyJCfLvmGSUuywoPDT4yQp9Ek5DDu8RoWzez1UJjMHM',
+	},
 };
 
 export default function RootLayout({
