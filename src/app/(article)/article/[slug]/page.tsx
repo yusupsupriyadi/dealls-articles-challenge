045653ecdfc0cb70031d36ba0320c4a9a2756900
yusup supriyadi <yusupsupriyadistudio@gmail.com>
2024-10-06 16:53:17 +0700

    feat: article detail

--- src/app/(article)/article/[slug]/page.tsx
+++ src/app/(article)/article/[slug]/page.tsx
@@ -1,65 +1,110 @@
-import { Eye, ThumbsUp } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
+import { Metadata } from 'next';
+import type { Article } from '@/components/interface/Article';
+import { ArticleStats } from './partials/ArticleStats';
+import { fetchData } from '@/services/fetch';
+
+async function getArticleData(slug: string): Promise<Article> {
+	const res = await fetchData<Article>(`/posts/${slug}`);
+	return res;
+}
+
+async function getRecommendedArticles(): Promise<Article[]> {
+	const response = await fetchData<{ posts: Article[] }>(`/posts`, {
+		sortBy: 'views',
+		order: 'desc',
+		limit: '4',
+	});
+	return response.posts;
+}
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { slug: string };
+}): Promise<Metadata> {
+	const [data] = await Promise.all([
+		getArticleData(params.slug),
+		getRecommendedArticles(),
+	]);
+	return {
+		title: data.title,
+		description: data.body.substring(0, 160),
+		twitter: {
+			card: 'summary_large_image',
+		},
+	};
+}
 
 export default async function Article({
 	params,
 }: {
 	params: { slug: string };
 }) {
-	const dataArticles = async () => {
-		const res = await fetch(`https://dummyjson.com/posts/${params.slug}`);
-		const data = await res.json();
-		return data;
-	};
-
-	const data = await dataArticles();
+	const [data, recommendedArticles] = await Promise.all([
+		getArticleData(params.slug),
+		getRecommendedArticles(),
+	]);
 
 	return (
-		<div className='mx-auto py-8'>
+		<article className='mx-auto py-8'>
 			<h1 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-6'>
 				{data.title}
 			</h1>
-			<section className='flex items-center gap-4 border-y border-gray-500 py-4 mb-8'>
-				<div className='flex items-center gap-2'>
-					<Eye className='w-3 h-3 md:w-4 md:h-4 text-gray-400' />
-					<p className='text-xs md:text-sm text-gray-400'>
-						{data.views} views
-					</p>
-				</div>
-				<div className='flex items-center gap-2'>
-					<ThumbsUp className='w-3 h-3 md:w-4 md:h-4 text-gray-400' />
-					<p className='text-xs md:text-sm text-gray-400'>
-						{data.reactions.likes} likes
-					</p>
-				</div>
-			</section>
-			<p className='text-base md:text-lg leading-relaxed'>{data.body}</p>
+			<ArticleStats
+				title={data.title}
+				views={data.views}
+				likes={data.reactions.likes}
+			/>
+
+			<div className='text-base md:text-lg leading-relaxed'>
+				{/* Use dangerouslySetInnerHTML if the content contains HTML */}
+				<p dangerouslySetInnerHTML={{ __html: data.body }} />
+			</div>
+
 			<div className='flex items-center gap-2 mt-8'>
-				{data.tags.map((tag: string) => (
+				{data.tags.map((tag) => (
 					<Link
 						href={`/?category=${tag}`}
 						key={tag}
-						className='text-xs md:text-sm bg-gray-200 px-3 py-1 rounded-2xl text-gray-700 capitalize'>
+						className='text-xs md:text-sm bg-gray-100 dark:bg-gray-800 px-3 py-2 rounded-3xl text-gray-800 dark:text-gray-300 capitalize'>
 						{tag}
 					</Link>
 				))}
 			</div>
+			<ArticleStats
+				className='mt-8 border-none'
+				title={data.title}
+				views={data.views}
+				likes={data.reactions.likes}
+			/>
 
-			<section className='flex items-center gap-4  py-4 mb-8'>
-				<div className='flex items-center gap-2'>
-					<Eye className='w-3 h-3 md:w-4 md:h-4 text-gray-400' />
-					<p className='text-xs md:text-sm text-gray-400'>
-						{data.views} views
-					</p>
-				</div>
-				<div className='flex items-center gap-2'>
-					<ThumbsUp className='w-3 h-3 md:w-4 md:h-4 text-gray-400' />
-					<p className='text-xs md:text-sm text-gray-400'>
-						{data.reactions.likes} likes
-					</p>
-				</div>
-			</section>
-		</div>
+			<h2 className='text-2xl font-bold mt-8'>Rekomendasi dari Dealls</h2>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-8 mt-8'>
+				{recommendedArticles.map((article: Article) => (
+					<Link
+						href={`/article/${article.id}`}
+						key={article.id}
+						className='block'>
+						<div>
+							<h3 className='font-semibold text-lg mb-2'>
+								{article.title}
+							</h3>
+							<p className='text-sm text-gray-600 dark:text-gray-400'>
+								{article.body.substring(0, 100)}...
+							</p>
+
+							<ArticleStats
+								className='!mt-1 !mb-0 !px-0 border-none'
+								title={article.title}
+								views={article.views}
+								likes={article.reactions.likes}
+							/>
+						</div>
+					</Link>
+				))}
+			</div>
+		</article>
 	);
 }
