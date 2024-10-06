814211f7bb8546949cc6e37570cf90d3be7c4ead
yusup supriyadi <yusupsupriyadistudio@gmail.com>
2024-10-06 16:53:03 +0700

    feat: article landing page

--- src/app/(article)/page.tsx
+++ src/app/(article)/page.tsx
@@ -1,31 +1,43 @@
-import SliderList from '@/components/SliderList';
-import ArticlesSlider from '../../components/ArticlesSlider';
-import ListArticles from '../../components/ListArticles';
+import SliderList from '@/components/ui/SliderList';
+import ArticlesSlider from '@/components/ui/ArticlesSlider';
+import ListArticles from '@/components/ui/ListArticles';
+import { Article } from '@/components/interface/Article';
+import { fetchData } from '@/services/fetch';
+
+async function getCategories(): Promise<string[]> {
+	return fetchData<string[]>(`/posts/tag-list`);
+}
+
+async function getHighlightedArticles(): Promise<Article[]> {
+	const response = await fetchData<{ posts: Article[] }>(`/posts`, {
+		sortBy: 'views',
+		order: 'desc',
+		limit: '5',
+	});
+	return response.posts;
+}
 
 export default async function Home({
 	searchParams,
 }: {
-	searchParams: { category: string };
+	searchParams: { category?: string };
 }) {
 	const category = searchParams.category;
 
-	const dataCategories = async () => {
-		const res = await fetch('https://dummyjson.com/posts/tag-list');
-		const data = await res.json();
-		return data;
-	};
-
-	const categories = await dataCategories();
+	const [categories, highlightedArticles] = await Promise.all([
+		getCategories(),
+		getHighlightedArticles(),
+	]);
 
 	return (
 		<>
-			<ArticlesSlider />
+			<ArticlesSlider data={highlightedArticles as Article[]} />
 			<SliderList
 				data={categories}
 				active={category}
 				className='mt-4'
 			/>
-			<ListArticles params={{ category }} />
+			<ListArticles params={{ category: category || '' }} />
 		</>
 	);
 }
