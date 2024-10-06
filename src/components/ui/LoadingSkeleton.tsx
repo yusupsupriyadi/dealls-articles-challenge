70c2d02488b62b98a09b9043a8a909a96deb83a8
yusup supriyadi <yusupsupriyadistudio@gmail.com>
2024-10-06 16:58:37 +0700

    feat(component/ui): loading skeleton

---
+++ src/components/ui/LoadingSkeleton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function LoadingSkeleton() {
+	return (
+		<div className=' py-6 md:py-8'>
+			<div className='animate-pulse'>
+				<div className='h-6 w-1/2 bg-gray-300/50 rounded-sm mb-2'></div>
+				<div className='h-20 w-full bg-gray-300/50 rounded-sm mb-2'></div>
+				<div className='flex items-center gap-4'>
+					<div className='flex items-center gap-1'>
+						<div className='h-4 w-4 bg-gray-300/50 rounded-full'></div>
+						<div className='h-3 w-10 bg-gray-300/50 rounded-sm'></div>
+					</div>
+					<div className='flex items-center gap-1'>
+						<div className='h-4 w-4 bg-gray-300/50 rounded-full'></div>
+						<div className='h-3 w-10 bg-gray-300/50 rounded-sm'></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
